ce576117de355ea56bef22fe6ba90b6b212e47a7
Joe Attardi <jattardi@gmail.com>
2025-04-04 06:55:53 -0400

    add more copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 export default function Home() {
     return (
-        <div>
-            <section className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-4">
+            <section className="flex flex-col md:flex-row justify-center items-center gap-4">
                 <p className="w-2/3 text-4xl md:text-6xl text-center font-bold">
                     Hi, I&apos;m Joe! I turn ideas into interactive web applications.
                 </p>
@@ -14,6 +14,11 @@ export default function Home() {
                     className="rounded-full border-4 border-zinc-200 shadow-lg w-48"
                 />
             </section>
+            <p className="w-2/3 text-center text-xl">
+                I&apos;ve been a software developer for over 20 years, specializing in front-end
+                development and creating user-friendly, accessible interfaces. I also write articles
+                and books on web development to share knowledge with the tech community.
+            </p>
         </div>
     );
 }
