a869c51a7cbd7169370aae2a27c87073a0c7da76
Joe Attardi <jattardi@gmail.com>
2025-04-04 00:28:39 -0400

    add motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ export default function Home() {
     return (
         <div>
             <section className="flex flex-col items-center gap-4">
-              <p className="w-2/3 text-4xl md:text-6xl text-center font-medium">Hi, I&apos;m Joe! I turn ideas into interactive web applications.</p>
-              <Image 
-                src={joe} 
-                alt="Joe Attardi"
-                className="rounded-full border-4 border-zinc-200 shadow-lg w-48" 
-              />
+                <p className="w-2/3 text-4xl md:text-6xl text-center font-medium">
+                    Hi, I&apos;m Joe! I turn ideas into interactive web applications.
+                </p>
+                <Image
+                    src={joe}
+                    alt="Joe Attardi"
+                    className="rounded-full border-4 border-zinc-200 shadow-lg w-48"
+                />
             </section>
         </div>
     );
