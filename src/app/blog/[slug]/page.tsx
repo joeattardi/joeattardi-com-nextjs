95719e7bdb9c8f12b7565aeb1345a21bfd638038
Joe Attardi <jattardi@gmail.com>
2025-04-05 10:22:07 -0400

    Add more blog content

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -16,13 +16,18 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
 
     return (
         <div className="flex flex-col gap-8 items-center w-[90vw] md:max-w-prose mx-auto">
-            {post.data.image &&<img src={post.data.image} alt={post.data.title} className="rounded-xl shadow-lg" />}
+            {post.data.image && (
+                <div className="flex flex-col gap-2">
+                    <img src={post.data.image} alt={post.data.title} className="rounded-xl shadow-lg" />
+                    <small>{post.data.imageCredit}</small>
+                </div>
+            )}
             <div className="flex flex-col bg-slate-200 p-4 rounded-xl">
                 <h2 className="text-4xl font-bold">{post.data.title}</h2>
                 <h3 className="text-lg text-zinc-500">{post.data.description}</h3>
             </div>
             <div className="text-zinc-600 italic self-start">Joe Attardi</div>
-            <article className="prose md:prose-lg prose-h2:my-2 prose-h3:my-2 prose-li:m-0 w-[90vw] md:w-full">
+            <article className="prose md:prose-lg prose-h2:my-2 prose-h3:my-2 prose-li:m-0 prose-img:mx-auto w-[90vw] md:w-full">
                 <MDXRemote source={post.content} options={options} />
             </article>
         </div>
