bb456145c6c4f15615609566c38325bb88f95057
Joe Attardi <jattardi@gmail.com>
2025-04-04 21:54:56 -0400

    Add byline

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -16,11 +16,12 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
 
     return (
         <div className="flex flex-col gap-8 items-center w-[90vw] md:max-w-prose mx-auto">
-            <img src={post.data.image} alt={post.data.title} className="rounded-xl shadow-lg" />
+            {post.data.image &&<img src={post.data.image} alt={post.data.title} className="rounded-xl shadow-lg" />}
             <div className="flex flex-col bg-slate-200 p-4 rounded-xl">
                 <h2 className="text-4xl font-bold">{post.data.title}</h2>
                 <h3 className="text-lg text-zinc-500">{post.data.description}</h3>
             </div>
+            <div className="text-zinc-600 italic self-start">Joe Attardi</div>
             <article className="prose md:prose-lg prose-h2:my-2 prose-h3:my-2 prose-li:m-0 w-[90vw] md:w-full">
                 <MDXRemote source={post.content} options={options} />
             </article>
