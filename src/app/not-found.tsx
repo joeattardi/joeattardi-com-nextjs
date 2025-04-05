ac1f216b38c6a2196432f28e681cc1e8fe759435
Joe Attardi <jattardi@gmail.com>
2025-04-04 21:34:55 -0400

    Fix page height on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export const runtime = 'edge';
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center">
             <div className="text-9xl"><PiMaskSadDuotone /></div>
             <h2 className="text-4xl">Not Found</h2>
             <p className="text-lg">The page you are looking for does not exist.</p>
