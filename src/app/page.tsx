68a32f54cf62ea7f4b74a80d3a7ab3b20bc195fd
Joe Attardi <jattardi@gmail.com>
2025-04-12 20:50:35 -0400

    Change profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import joe from '@/images/avatar.jpg';
+import joe from '@/images/image3.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import SocialButton from '@/components/SocialButton';
